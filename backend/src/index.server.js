4e5c871c4e87d138ea6a01602be766ccc13ba8ec
Nathan Andrade <nathandulino2003@gmail.com>
2020-11-17 23:27:09 -0200

    configs

--- backend/src/index.server.js
+++ backend/src/index.server.js
@@ -1,25 +1,29 @@
 const express = require('express');
-const env = require('dotenv')
+const env = require('dotenv');
 const app = express();
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+//routes
+const userRoutes = require('./routes/user');
 
 // env
 env.config();
 
+// mongoDb
+//mongodb+srv://root:<password>@cluster0.vcg4x.mongodb.net/<dbname>?retryWrites=true&w=majority
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.vcg4x.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+ {
+   useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+).then(() => {
+  console.log('Database connected');
+});
+
 app.use(bodyParser());
-
-app.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Hello from Server'
-  })
-})
-
-app.post('/data', (req, res, next) => {
-  res.status(200).json({
-    message: req.body
-  })
-})
+app.use('/api', userRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`)
---
+++ backend/src/routes/user.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/singin', (req, res) => {
+
+});
+
+router.post('/singup', (req, res) => {
+  
+});
+
+module.exports = router;
