f61360883597560baecae4ba0cec6d6997a0e749
Nathan Andrade <nathandulino2003@gmail.com>
2020-11-26 22:51:16 -0200

    add components

---
+++ admin-app/src/components/layout/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Header/index';
+
+function Layout(props){
+  return (
+    <>
+      <Header />
+      {props.children}
+    </>
+  )
+}
+
+export default Layout;
