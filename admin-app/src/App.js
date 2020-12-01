db7bd8ed2452550143f5ba69f6af3015a9a6e176
Nathan Andrade <nathandulino2003@gmail.com>
2020-11-30 23:53:51 -0200

    updates

--- admin-app/src/App.js
+++ admin-app/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import './App.css';
 import Home from './containers/Home/index';
 import Signin from './containers/Signin/index';
 import Signup from './containers/Signup/index';
+
+import './App.css';
 
 function App() {
   return (
