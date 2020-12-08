d507a13c72919d6f895ecac24a89b789dfd2c5cb
Nathan Andrade <nathandulino2003@gmail.com>
2020-12-07 23:23:37 -0200

    updates

--- backend/src/common-middleware/index.js
+++ backend/src/common-middleware/index.js
@@ -21,7 +21,7 @@ exports.userMiddleware = (req, res, next) => {
 
 exports.adminMiddleware = (req, res, next) => {
   if(req.user.role !== 'admin'){
-    return res.status(400).json({ message: 'Access denied' })
+    return res.status(400).json({ message: 'Admin access denied' })
   }
   next();
 }
