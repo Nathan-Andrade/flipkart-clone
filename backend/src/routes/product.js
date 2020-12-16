c5d94debc909002b1b5fe28ef6c0decff67a7dd8
Nathan Andrade <nathandulino2003@gmail.com>
2020-12-15 22:58:52 -0200

    product creation

--- backend/src/routes/product.js
+++ backend/src/routes/product.js
@@ -3,10 +3,23 @@ const { requireSignin, adminMiddleware } = require('../common-middleware');
 //const { addCategory, getCategory } = require('../controller/category');
 const { createProduct } = require('../controller/product');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' })
+const shortid = require('shortid');
+const path = require('path');
 const router = express.Router();
 
-router.post('/product/create', requireSignin, adminMiddleware, upload.single('productPicture'), createProduct);
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(path.dirname(__dirname), 'uploads'))
+  },
+  filename: function (req, file, cb){
+    cb(null, shortid.generate() + '-' + file.originalname)
+  }
+})
+
+const upload = multer({ storage });
+
+router.post('/product/create', requireSignin, adminMiddleware, upload.array('productPicture'), createProduct);
 //router.get('/category/getcategory', getCategory);
 
 module.exports = router;
