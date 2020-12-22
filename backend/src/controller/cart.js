fecf353ffa41865528ff7814497aa6215b974e06
Nathan Andrade <nathandulino2003@gmail.com>
2020-12-21 23:11:29 -0200

    updates

--- backend/src/controller/cart.js
+++ backend/src/controller/cart.js
@@ -1,5 +1,15 @@
 const Cart = require('../models/cart');
 
 exports.addItemToCart = (req, res) => {
-  res.json({ message: 'Cart' })
+  const cart = new Cart({
+    user: req.user._id,
+    cartItems: req.body.cartItems
+  })
+
+  cart.save((error, cart) => {
+    if(error) return res.status(400).json({ error });
+    if(cart){
+      return res.status(201).json({ cart })
+    }
+  })
 }
